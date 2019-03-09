c7914e6e1d6500474464e33c844271bf3416110f
Cemil Oten <cemil.oten@gmail.com>
2019-03-09 22:48:13 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+typedef void (*GLFWerrorfun)(int, const char*);
+void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int main() {
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Space Invaders", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    int gl_version[2] { -1, 1 };
+    glGetIntegerv(GL_MAJOR_VERSION, &gl_version[0]);
+    glGetIntegerv(GL_MINOR_VERSION, &gl_version[1]);
+    printf("Using OpenGL: %d.%d\n", gl_version[0], gl_version[1]);
+
+    glClearColor(1.0, 0.5, 0.0, 1.0);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
